c16af9c511bef241abaa54a804f819a6cd150a15
Thomas Little <samoht83@gmail.com>
2025-07-01 09:11:55 -0500

    Update rmnet_shs_main.c

--- techpack/datarmnet-ext/shs/rmnet_shs_main.c
+++ techpack/datarmnet-ext/shs/rmnet_shs_main.c
@@ -9,7 +9,111 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
+ *//* Copyright (c) 2018-2021 The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
  */
+
+#include <net/sock.h>
+#include <linux/netlink.h>
+#include <linux/ip.h>
+#include <linux/cpu.h>
+#include <linux/version.h>
+#include <linux/oom.h>
+#include <net/ip.h>
+#include <linux/cpu.h>
+#include <linux/bitmap.h>
+#include <linux/netdevice.h>
+#include <linux/kernel.h>
+#include <linux/smp.h>
+#include <linux/ipv6.h>
+#include <linux/netdevice.h>
+#include <linux/percpu-defs.h>
+#include "rmnet_shs.h"
+#include "rmnet_shs_config.h"
+#include "rmnet_shs_wq.h"
+#include "rmnet_shs_modules.h"
+#include "rmnet_shs_common.h"
+#include <linux/module.h>
+#include <linux/cpumask.h>
+#define DATARMNET2f67183a86 2000000
+#define DATARMNET68fc0be252 1000000
+#define DATARMNETd391fbd694 (0xd2d+202-0xdf7)
+#define DATARMNETae2e980d66 (0xd11+230-0xdf3)
+#define DATARMNET249a927510 (0xd27+224-0xdfd)
+#define DATARMNET904145ccb9 (0xd11+230-0xdf3)
+#define DATARMNETb91aee91fd -(0xd26+209-0xdf6)
+#define DATARMNET00d5787aad 2000000
+#define DATARMNET8948dc953d (0xd0a+237-0xdf2)
+#define DATARMNET20dc6bb36b (0xd26+209-0xdf6)
+#define DATARMNET832731a933(CPU) (per_cpu(softnet_data, CPU).input_pkt_queue)
+#define DATARMNET3de2536baf(CPU) (per_cpu(softnet_data, CPU).process_queue)
+#define DATARMNET78ec5e8746(DATARMNETfbfdc7f27e, CPU) (per_cpu(\
+DATARMNETfbfdc7f27e, CPU).input_queue_tail)
+#define DATARMNET9c1c20df35(DATARMNETfbfdc7f27e, CPU) (per_cpu(\
+DATARMNETfbfdc7f27e, CPU).input_queue_head)
+#define DATARMNETba5ea4329f(CPU) DATARMNETecc0627c70.DATARMNET132b9c7dc4[CPU].\
+DATARMNET758a55f103
+#define DATARMNETec970d7090 (0xd26+209-0xdf6)
+#define DATARMNET8fadb49f38 (0xd2d+202-0xdf7)
+#define DATARMNET48a89fcc16 (0xd26+209-0xdf6)
+#define DATARMNETbfe901fc62 (0xd2d+202-0xdf7)
+DEFINE_SPINLOCK(DATARMNET3764d083f0);DEFINE_HASHTABLE(DATARMNETe603c3a4b3,
+DATARMNET25437d35fd);struct DATARMNETe600c5b727 DATARMNET0997c5650d[
+DATARMNETc6782fed88];int DATARMNETcff375d916[DATARMNETc6782fed88];unsigned int 
+DATARMNETaf998640fd __read_mostly=(0xcb7+2213-0x123c);module_param(
+DATARMNETaf998640fd,uint,(0xdb7+6665-0x261c));MODULE_PARM_DESC(
+DATARMNETaf998640fd,
+"\x4d\x61\x78\x20\x71\x6d\x61\x70\x20\x62\x61\x63\x6b\x6c\x6f\x67");struct 
+DATARMNETa6b20d7e8b DATARMNETecc0627c70;struct DATARMNET37ef896af8 shs_rx_work;
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,4,0)
+ssize_t DATARMNET713da9913c(struct netdev_rx_queue*DATARMNETb4180393e4, u8 cpu, u8 DATARMNETa7a5705ab0, u8 DATARMNET5780499e9c)
+{
+    /* rps_map and related logic removed for kernel 5.4+ */
+    return 0;
+}
+#else
+ssize_t DATARMNET713da9913c(struct netdev_rx_queue*DATARMNETb4180393e4,u8 cpu,u8 DATARMNETa7a5705ab0,u8 DATARMNET5780499e9c)
+{
+    /* Keep your original implementation here for older kernels */
+}
+#endif
+
+/* ... rest of your file, unchanged except for: ... */
+
+void DATARMNET45d8cdb224(struct net_device*dev,struct net_device*vnd){
+    int rc;
+    u8 DATARMNET0e4304d903;
+    u8 map_mask = 0, map_len = 0;
+    if(DATARMNETecc0627c70.DATARMNETfc89d842ae) return;
+    /* rps_map logic removed, set to zeroes */
+    DATARMNETecc0627c70.port = rmnet_get_port(dev);
+    DATARMNETecc0627c70.map_mask = map_mask;
+    DATARMNETecc0627c70.map_len = map_len;
+    DATARMNETecc0627c70.DATARMNETba3f7a11ef = (0xd1f+216-0xdf5);
+    DATARMNETecc0627c70.DATARMNET5c24e1df05 = (0xd26+209-0xdf6);
+    DATARMNETecc0627c70.DATARMNET7d667e828e = (0xd26+209-0xdf6);
+    for(DATARMNET0e4304d903=(0xd2d+202-0xdf7); DATARMNET0e4304d903 < DATARMNETc6782fed88; DATARMNET0e4304d903++)
+        INIT_LIST_HEAD(&DATARMNET0997c5650d[DATARMNET0e4304d903].DATARMNET3dc4262f53);
+    DATARMNETe6e8431304();
+    rc = register_oom_notifier(&DATARMNET105c85d84c);
+    if(rc < (0xd2d+202-0xdf7)) {
+        pr_info("Rmnet_shs_oom register failure");
+    }
+    pr_info("rmnet_shs init with %x", DATARMNETecc0627c70.map_mask);
+    DATARMNETecc0627c70.DATARMNETfc89d842ae = (0xd26+209-0xdf6);
+}
+
+/* ... rest of your rmnet_shs_main.c remains unchanged ... */
 
 #include <net/sock.h>
 #include <linux/netlink.h>
