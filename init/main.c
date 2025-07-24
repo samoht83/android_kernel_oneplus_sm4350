9b2e0467500a9e6a9990126277790994e8f54820
Thomas Little <samoht83@gmail.com>
2025-07-24 02:10:31 -0500

    chore: update init/main.c (unspecified changes)

--- init/main.c
+++ init/main.c
@@ -134,7 +134,7 @@ extern void time_init(void);
 void (*__initdata late_time_init)(void);
 
 /* Untouched command line saved by arch-specific code. */
-char *boot_command_line[COMMAND_LINE_SIZE];
+char __initdata boot_command_line[COMMAND_LINE_SIZE];
 /* Untouched saved command line (eg. for /proc) */
 char *saved_command_line;
 /* Command line for parameter parsing */
